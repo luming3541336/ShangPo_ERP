0656a96c99cd96ad033d11a1d0cf56827252045f
盧彥儒 <a3541336@gmail.com>
2019-09-20 17:56:57 +0800

    新增使用者登入介面 新增Material Skin 介面 測試伸縮按鈕

--- 上博企業管理系統/View/Main_Form.vb
+++ 上博企業管理系統/View/Main_Form.vb
@@ -1,116 +1,25 @@
-﻿Imports System.Data.SqlClient
-Imports System.IO
-Imports Microsoft.VisualBasic.FileIO
+﻿
+Imports MaterialSkin
 
 Public Class Main_Form
-    Const USER_RECORD_FILE As String = "..\Log\Application.log"
-    Private oOriginalRegion As Region = Nothing
-    Private bFormDragging As Boolean = False
-    Private oPointClicked As Point
-
-
     '主畫面讀取
     '   1.先去判斷先前使用者是否有設定"記住我"，並且有正確登入，如有，資料將會記錄在Application.log內
     '   2.判斷userIDText及passwordText是否有空，有的話將會顯示預設字樣
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim skinManager = MaterialSkinManager.Instance
+        skinManager.AddFormToManage(Me)
+        skinManager.Theme = MaterialSkinManager.Themes.DARK
+        skinManager.ColorScheme = New ColorScheme(Primary.Teal700, Primary.Teal700, Primary.Teal700, Accent.Teal700, TextShade.WHITE)
         Try
             VerText.Text = Deployment.Application.ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString()
         Catch ex As Exception
             VerText.Text = "開發程式階段"
         End Try
-
-        '使用者如有按下"記住我"按鈕將會讀取資料
-        If File.Exists(USER_RECORD_FILE) Then
-            Dim fileReader As TextFieldParser = New TextFieldParser(USER_RECORD_FILE) With {
-                .TextFieldType = FieldType.Delimited
-            }
-            fileReader.SetDelimiters(",")
-            Dim currentRow As String()
-            Do While Not fileReader.EndOfData
-                currentRow = fileReader.ReadFields()
-                MemoryUser = currentRow(0)
-                UserID = currentRow(1)
-                Password = currentRow(2)
-                readBoard = currentRow(3)
-
-                recordCheckBox.Checked = MemoryUser
-                userIDText.Text = UserID
-                passwordText.Text = Password
-            Loop
-        Else
-            Directory.CreateDirectory("..\Log")
-            Dim fs As FileStream = File.Create(USER_RECORD_FILE)
-            fs.Close()
-        End If
-        If userIDText.Text = "" Then
-            userIDText.Text = "使用者帳號"
-            userIDText.ForeColor = Color.Silver
-        End If
-        If passwordText.Text = "" Then
-            passwordText.Text = "使用者密碼"
-            passwordText.ForeColor = Color.Silver
+        Timer1.Start()
+        If readBoard = False Then
+            BillBoardForm.Show()
         End If
 
-    End Sub
-
-    Private Sub userIDText_Enter(sender As Object, e As EventArgs) Handles userIDText.Enter, passwordText.Enter
-        sender.text = ""
-        sender.ForeColor = Color.Black
-    End Sub
-
-    Private Sub userIDText_Leave(sender As Object, e As EventArgs) Handles userIDText.Leave, passwordText.Leave
-        If sender.text = "" Then
-            sender.Text = IIf(sender.name = "userIDText", "使用者帳號", "使用者密碼")
-            sender.ForeColor = Color.Silver
-        End If
-    End Sub
-    Private Sub OKButton_Click(sender As Object, e As EventArgs) Handles OKButton.Click
-
-        Dim controller As Main_Controller = New Main_Controller
-        Dim loginData As Data_User_Model.LoginData = controller.Select_User(userIDText.Text, passwordText.Text)
-        If loginData.ToString <> Nothing Then
-            UserName = loginData.UserName
-            Access = loginData.Access
-            LoginID = loginData.LoginID
-            L0 = loginData.L0
-            L1 = loginData.L1
-            L2 = loginData.L2
-            L3 = loginData.L3
-            L4 = loginData.L4
-            L5 = loginData.L5
-            L6 = loginData.L6
-            Dim userControl As List_Control_View = New List_Control_View()
-            Timer1.Start()
-            RevisePasswordLabel.Visible = True
-            MainPanel.Controls.Add(userControl)
-            userControl.Show()
-            If readBoard = False Then
-                BillBoardForm.Show()
-            End If
-            userControl.Dock = DockStyle.Bottom
-            loginTableLayout.Hide()
-            '如果有按下記住我則將資料紀錄
-            MemoryUser = recordCheckBox.Checked
-            UserID = userIDText.Text
-            Password = passwordText.Text
-            readBoard = readBoard
-            If recordCheckBox.Checked Then
-                File.WriteAllText(USER_RECORD_FILE, "True ," & userIDText.Text & "," & passwordText.Text & "," & readBoard)
-            Else
-                File.WriteAllText(USER_RECORD_FILE, "False,,," & readBoard)
-            End If
-        Else
-            MsgBox("帳號或密碼錯誤")
-        End If
-
-    End Sub
-
-    Private Sub passwordText_TextChanged(sender As Object, e As EventArgs) Handles passwordText.TextChanged
-        If passwordText.Text = "使用者密碼" Then
-            passwordText.PasswordChar = Nothing
-        Else
-            passwordText.PasswordChar = "*"
-        End If
     End Sub
 
     Private Sub VerText_Click(sender As Object, e As EventArgs) Handles VerText.Click
@@ -132,6 +41,6 @@ Public Class Main_Form
     Private Sub RevisePasswordLabel_Click(sender As Object, e As EventArgs) Handles RevisePasswordLabel.Click
         Dim reviseForm As Set_PasswordRevise_Form = New Set_PasswordRevise_Form
         reviseForm.ShowDialog()
-
     End Sub
+
 End Class
---
+++ 上博企業管理系統/View/UserControl.vb
@@ -0,0 +1,22 @@
+﻿Public Class UserControl
+    Dim a = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If a = 0 Then
+            FlowLayoutPanel2.Visible = True
+            a = 1
+        Else
+            FlowLayoutPanel2.Visible = False
+            a = 0
+        End If
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
+
+    Private Sub UserControl_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FlowLayoutPanel2.Visible = False
+    End Sub
+End Class
