50e4c1b2d28966a7a7129376945f3bf0bf6dc62a
盧彥儒 <a3541336@gmail.com>
2019-09-24 18:00:03 +0800

    導覽按鈕更新

--- 上博企業管理系統/View/UserControl.Designer.vb
+++ 上博企業管理系統/View/UserControl.Designer.vb
@@ -22,6 +22,8 @@ Partial Class UserControl
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UserControl))
         Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Panel1 = New System.Windows.Forms.Panel()
@@ -40,6 +42,8 @@ Partial Class UserControl
         Me.Panel4 = New System.Windows.Forms.Panel()
         Me.Button12 = New System.Windows.Forms.Button()
         Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.FlowLayoutPanel1.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
@@ -73,6 +77,9 @@ Partial Class UserControl
         Me.Button1.FlatAppearance.BorderSize = 0
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.ImageIndex = 1
+        Me.Button1.ImageList = Me.ImageList1
         Me.Button1.Location = New System.Drawing.Point(0, 0)
         Me.Button1.Margin = New System.Windows.Forms.Padding(0)
         Me.Button1.Name = "Button1"
@@ -102,7 +109,7 @@ Partial Class UserControl
         Me.Button3.Location = New System.Drawing.Point(44, 29)
         Me.Button3.Margin = New System.Windows.Forms.Padding(0)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(190, 31)
+        Me.Button3.Size = New System.Drawing.Size(178, 31)
         Me.Button3.TabIndex = 2
         Me.Button3.Text = "新增估價"
         Me.Button3.UseVisualStyleBackColor = False
@@ -116,7 +123,7 @@ Partial Class UserControl
         Me.Button2.Location = New System.Drawing.Point(44, 0)
         Me.Button2.Margin = New System.Windows.Forms.Padding(0)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(190, 31)
+        Me.Button2.Size = New System.Drawing.Size(178, 31)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "估價總覽"
         Me.Button2.UseVisualStyleBackColor = False
@@ -130,7 +137,7 @@ Partial Class UserControl
         Me.Button7.Location = New System.Drawing.Point(44, 60)
         Me.Button7.Margin = New System.Windows.Forms.Padding(0)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(190, 31)
+        Me.Button7.Size = New System.Drawing.Size(178, 31)
         Me.Button7.TabIndex = 2
         Me.Button7.Text = "列印報價單"
         Me.Button7.UseVisualStyleBackColor = False
@@ -141,6 +148,9 @@ Partial Class UserControl
         Me.Button4.FlatAppearance.BorderSize = 0
         Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button4.ImageKey = "baseline_right.png"
+        Me.Button4.ImageList = Me.ImageList1
         Me.Button4.Location = New System.Drawing.Point(0, 122)
         Me.Button4.Margin = New System.Windows.Forms.Padding(0)
         Me.Button4.Name = "Button4"
@@ -151,12 +161,13 @@ Partial Class UserControl
         '
         'Panel2
         '
+        Me.Panel2.Controls.Add(Me.Button14)
         Me.Panel2.Controls.Add(Me.Button5)
         Me.Panel2.Controls.Add(Me.Button6)
         Me.Panel2.Location = New System.Drawing.Point(0, 153)
         Me.Panel2.Margin = New System.Windows.Forms.Padding(0)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(222, 62)
+        Me.Panel2.Size = New System.Drawing.Size(222, 93)
         Me.Panel2.TabIndex = 1
         Me.Panel2.Visible = False
         '
@@ -169,7 +180,7 @@ Partial Class UserControl
         Me.Button5.Location = New System.Drawing.Point(44, 31)
         Me.Button5.Margin = New System.Windows.Forms.Padding(0)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(190, 31)
+        Me.Button5.Size = New System.Drawing.Size(178, 31)
         Me.Button5.TabIndex = 2
         Me.Button5.Text = "案件查詢"
         Me.Button5.UseVisualStyleBackColor = False
@@ -183,7 +194,7 @@ Partial Class UserControl
         Me.Button6.Location = New System.Drawing.Point(44, 0)
         Me.Button6.Margin = New System.Windows.Forms.Padding(0)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(190, 31)
+        Me.Button6.Size = New System.Drawing.Size(178, 31)
         Me.Button6.TabIndex = 1
         Me.Button6.Text = "案件總覽"
         Me.Button6.UseVisualStyleBackColor = False
@@ -194,7 +205,10 @@ Partial Class UserControl
         Me.Button8.FlatAppearance.BorderSize = 0
         Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button8.ForeColor = System.Drawing.Color.White
-        Me.Button8.Location = New System.Drawing.Point(0, 215)
+        Me.Button8.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button8.ImageIndex = 1
+        Me.Button8.ImageList = Me.ImageList1
+        Me.Button8.Location = New System.Drawing.Point(0, 246)
         Me.Button8.Margin = New System.Windows.Forms.Padding(0)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(222, 31)
@@ -206,7 +220,7 @@ Partial Class UserControl
         '
         Me.Panel3.Controls.Add(Me.Button9)
         Me.Panel3.Controls.Add(Me.Button10)
-        Me.Panel3.Location = New System.Drawing.Point(0, 246)
+        Me.Panel3.Location = New System.Drawing.Point(0, 277)
         Me.Panel3.Margin = New System.Windows.Forms.Padding(0)
         Me.Panel3.Name = "Panel3"
         Me.Panel3.Size = New System.Drawing.Size(222, 61)
@@ -222,7 +236,7 @@ Partial Class UserControl
         Me.Button9.Location = New System.Drawing.Point(44, 30)
         Me.Button9.Margin = New System.Windows.Forms.Padding(0)
         Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(190, 31)
+        Me.Button9.Size = New System.Drawing.Size(178, 31)
         Me.Button9.TabIndex = 2
         Me.Button9.Text = "應收帳款"
         Me.Button9.UseVisualStyleBackColor = False
@@ -236,7 +250,7 @@ Partial Class UserControl
         Me.Button10.Location = New System.Drawing.Point(44, 0)
         Me.Button10.Margin = New System.Windows.Forms.Padding(0)
         Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(190, 31)
+        Me.Button10.Size = New System.Drawing.Size(178, 31)
         Me.Button10.TabIndex = 1
         Me.Button10.Text = "應收應付總覽"
         Me.Button10.UseVisualStyleBackColor = False
@@ -247,7 +261,10 @@ Partial Class UserControl
         Me.Button11.FlatAppearance.BorderSize = 0
         Me.Button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button11.ForeColor = System.Drawing.Color.White
-        Me.Button11.Location = New System.Drawing.Point(0, 307)
+        Me.Button11.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button11.ImageIndex = 1
+        Me.Button11.ImageList = Me.ImageList1
+        Me.Button11.Location = New System.Drawing.Point(0, 338)
         Me.Button11.Margin = New System.Windows.Forms.Padding(0)
         Me.Button11.Name = "Button11"
         Me.Button11.Size = New System.Drawing.Size(222, 31)
@@ -259,7 +276,7 @@ Partial Class UserControl
         '
         Me.Panel4.Controls.Add(Me.Button12)
         Me.Panel4.Controls.Add(Me.Button13)
-        Me.Panel4.Location = New System.Drawing.Point(0, 338)
+        Me.Panel4.Location = New System.Drawing.Point(0, 369)
         Me.Panel4.Margin = New System.Windows.Forms.Padding(0)
         Me.Panel4.Name = "Panel4"
         Me.Panel4.Size = New System.Drawing.Size(222, 62)
@@ -275,7 +292,7 @@ Partial Class UserControl
         Me.Button12.Location = New System.Drawing.Point(44, 31)
         Me.Button12.Margin = New System.Windows.Forms.Padding(0)
         Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(190, 31)
+        Me.Button12.Size = New System.Drawing.Size(178, 31)
         Me.Button12.TabIndex = 2
         Me.Button12.Text = "修改密碼"
         Me.Button12.UseVisualStyleBackColor = False
@@ -289,10 +306,31 @@ Partial Class UserControl
         Me.Button13.Location = New System.Drawing.Point(44, 0)
         Me.Button13.Margin = New System.Windows.Forms.Padding(0)
         Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(190, 31)
+        Me.Button13.Size = New System.Drawing.Size(178, 31)
         Me.Button13.TabIndex = 1
         Me.Button13.Text = "帳戶設定"
         Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.SlateGray
+        Me.Button14.FlatAppearance.BorderSize = 0
+        Me.Button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button14.ForeColor = System.Drawing.Color.White
+        Me.Button14.Location = New System.Drawing.Point(44, 62)
+        Me.Button14.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(178, 31)
+        Me.Button14.TabIndex = 2
+        Me.Button14.Text = "庫存紀錄"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "baseline_Down.png")
+        Me.ImageList1.Images.SetKeyName(1, "baseline_right.png")
         '
         'UserControl
         '
@@ -330,4 +368,6 @@ Partial Class UserControl
     Friend WithEvents Panel4 As Panel
     Friend WithEvents Button12 As Button
     Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents ImageList1 As ImageList
 End Class
